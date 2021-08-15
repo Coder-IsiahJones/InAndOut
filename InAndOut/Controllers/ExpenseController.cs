306f0a43ee259b485525ed7d08706487a242a30a
Isiah Jones <isiah.jones33@outlook.com>
2021-08-14 22:00:55 -0400

    Add the Expense Model with View.

---
+++ InAndOut/Controllers/ExpenseController.cs
@@ -0,0 +1,42 @@
+﻿using InAndOut.Data;
+using InAndOut.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InAndOut.Controllers
+{
+    public class ExpenseController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ExpenseController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Expense> objectList = _context.Expenses;
+
+            return View(objectList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Expense obj)
+        {
+            _context.Expenses.Add(obj);
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
