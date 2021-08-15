6af4cc6d06cf51c9cf2b1806ef61fc3fd3ea68d9
Isiah Jones <isiah.jones33@outlook.com>
2021-08-14 23:00:28 -0400

    Update & Delete

--- InAndOut/Controllers/ExpenseController.cs
+++ InAndOut/Controllers/ExpenseController.cs
@@ -1,10 +1,12 @@
-﻿using InAndOut.Data;
-using InAndOut.Models;
-using Microsoft.AspNetCore.Mvc;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using InAndOut.Data;
+using InAndOut.Models;
 
 namespace InAndOut.Controllers
 {
@@ -16,32 +18,97 @@ namespace InAndOut.Controllers
         {
             _context = context;
         }
-        public IActionResult Index()
-        {
-            IEnumerable<Expense> objectList = _context.Expenses;
 
-            return View(objectList);
+        // GET: Expenses
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Expenses.ToListAsync());
         }
 
+        // GET: Expenses/Create
         public IActionResult Create()
         {
             return View();
         }
 
+        // POST: Expenses/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(Expense obj)
+        public async Task<IActionResult> Create([Bind("Id,ExpenseName,Amount")] Expense expense)
         {
             if (ModelState.IsValid)
             {
-                _context.Expenses.Add(obj);
+                _context.Add(expense);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(expense);
+        }
 
+
+        // GET-Update
+        public IActionResult Update(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _context.Expenses.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+        // POST-Update
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(Expense obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Expenses.Update(obj);
                 _context.SaveChanges();
-
                 return RedirectToAction("Index");
             }
-
             return View(obj);
+        }
+
+
+        // GET: Expenses/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var expense = await _context.Expenses
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (expense == null)
+            {
+                return NotFound();
+            }
+
+            return View(expense);
+        }
+
+        // POST: Expenses/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var expense = await _context.Expenses.FindAsync(id);
+            _context.Expenses.Remove(expense);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ExpenseExists(int id)
+        {
+            return _context.Expenses.Any(e => e.Id == id);
         }
     }
 }
