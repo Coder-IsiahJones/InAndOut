5b5996bcf30f36e6ca879f696078a4e79fdd40fb
Isiah Jones <isiah.jones33@outlook.com>
2021-08-14 21:06:13 -0400

    Add project files.

---
+++ InAndOut/Controllers/ItemController.cs
@@ -0,0 +1,31 @@
+﻿using InAndOut.Data;
+using InAndOut.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InAndOut.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Item> objectList = _context.Items;
+
+            return View(objectList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
